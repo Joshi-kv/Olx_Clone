432aad4db21c5d9d6404b17dd5ce5f4381501f56
JOshi kv <joshikv3705@gmail.com>
2023-02-23 12:05:58 +0530

    login signup page design

--- olx_clone/olx_clone/urls.py
+++ olx_clone/olx_clone/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Home.urls'))
+    path('',include('Home.urls')),
+    path('user/',include('User.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
