9a02243f446c2d64bc48e0666f5da160f8e1a022
JOshi kv <joshikv3705@gmail.com>
2023-02-23 10:30:48 +0530

    home page design

--- olx_clone/olx_clone/settings.py
+++ olx_clone/olx_clone/settings.py
@@ -51,6 +51,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+SESSION_COOKIE_SECURE = True
+
+
 ROOT_URLCONF = 'olx_clone.urls'
 
 TEMPLATES = [
@@ -118,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
