e4502a444e57a656b11e7d54edca2da359fb3de3
JOshi kv <joshikv3705@gmail.com>
2023-03-14 12:31:40 +0530

    chat page design

--- olx_clone/Chat/views.py
+++ olx_clone/Chat/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def chat_page(request):
-    return render(request,'chat.html')
+    product_image = request.GET.get('product_image')
+    owner_name = request.GET.get('owner_name')
+    context={
+        'product_image':product_image,
+        'owner_name':owner_name
+    }
+    return render(request, 'chat.html',context )
