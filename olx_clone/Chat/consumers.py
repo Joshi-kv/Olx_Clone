e5328b27cdfb7322b8b66abbcd0e0ba7582b01c5
JOshi kv <joshikv3705@gmail.com>
2023-03-21 11:22:58 +0530

    sending msg received on server back to client

--- olx_clone/Chat/consumers.py
+++ olx_clone/Chat/consumers.py
@@ -17,8 +17,20 @@ class chatConsumer(AsyncWebsocketConsumer):
     #it is called when message form submitted
     async def websocket_receive(self, event):
         print(f"Received {event}")
-
-    
+        #deserializing js string  into python objects 
+        received_msg = json.loads(event['text'])
+        message = received_msg.get('message')
+        
+        if not message : 
+            return False
+        response = {
+            'message':message
+        }
+        
+        #sending msg received on server to client
+        response_str = json.dumps(response) #converting python objects into json string
+        await self.send(response_str)
+            
     #it is called when connection disconnected
     async def websocket_disconnect(self,event):
         print("Disconnected",event)
